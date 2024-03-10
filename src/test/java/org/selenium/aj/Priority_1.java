fd8ae4a2f0e6674066bf4abd3d2e65a234f1214d
apoorv jain <apoorv127@gmail.com>
2024-03-10 13:31:00 +0530

    10th march

--- src/test/java/org/selenium/aj/Priority_1.java
+++ src/test/java/org/selenium/aj/Priority_1.java
@@ -1,13 +1,15 @@
 package org.selenium.aj;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Priority_1{
-    @Test(priority = 1)
+    @BeforeTest
     public void testLogin(){
         System.out.println("Login");
     }
-    @Test(priority = 2)
+    @AfterTest
     public void testCart(){
         System.out.println("Cart");
     }
